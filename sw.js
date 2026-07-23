16e52a581a0f33939611f432825baefffc18f7f8
serin <tpfls105@gmail.com>
2026-07-23 23:24:02 +0900

    update service worker cache

--- sw.js
+++ sw.js
@@ -6,7 +6,111 @@ const FILES_TO_CACHE = [
 "./",
 "./index.html",
 "./style.css",
+"./manifest.json",const CACHE_NAME = "pos-kpi-pro-v2";
+
+
+const FILES_TO_CACHE = [
+
+"./",
+"./index.html",
+"./style.css",
 "./manifest.json",
+"./icon.png",
+
+"./js/storage.js",
+"./js/calculator.js",
+"./js/report.js",
+"./js/ui.js",
+"./js/app.js"
+
+];
+
+
+
+self.addEventListener(
+"install",
+event => {
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(
+cache => {
+
+return cache.addAll(FILES_TO_CACHE);
+
+}
+
+)
+
+);
+
+});
+
+
+
+
+
+self.addEventListener(
+"activate",
+event => {
+
+event.waitUntil(
+
+caches.keys()
+.then(
+keys => {
+
+return Promise.all(
+
+keys.map(
+
+key => {
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+}
+
+)
+
+);
+
+}
+
+)
+
+);
+
+});
+
+
+
+
+
+self.addEventListener(
+"fetch",
+event => {
+
+event.respondWith(
+
+caches.match(event.request)
+.then(
+
+response => {
+
+return response || fetch(event.request);
+
+}
+
+)
+
+);
+
+});
 
 "./js/storage.js",
 "./js/calculator.js",
